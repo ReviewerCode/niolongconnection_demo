8039aed52d2d09d8b6875f995d5185f2fd26f113
ReviewerCode <39054111+ReviewerCode@users.noreply.github.com>
2018-05-08 04:12:43 +0800

    Delete ConnectionException.java

--- src/main/java/heart/beat/exam/exceptions/ConnectionException.java
+++
@@ -1,9 +0,0 @@
-package heart.beat.exam.exceptions;
-
-public class ConnectionException extends AbstractException {
-	private static final long serialVersionUID = 7199861496131767237L;
-
-	public ConnectionException(Throwable t) {
-		super(t);
-	}
-}
